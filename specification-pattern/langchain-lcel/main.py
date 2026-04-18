79616ad1f566ac8b6569def8c9321a5076ab8884
David Galera <raizen74@gmail.com>
2026-04-18 22:42:52 +0200

    langchain-LCEL add partial functions

--- specification-pattern/langchain-lcel/main.py
+++ specification-pattern/langchain-lcel/main.py
@@ -1,13 +1,14 @@
+import inspect
 from collections.abc import Callable
 from dataclasses import dataclass
-from functools import wraps
+from functools import partial, wraps
 from typing import Any
 
 
 # The [In, Out] syntax automatically creates TypeVars and marks the class as Generic
 @dataclass
 class Runnable[In, Out]:
-    func: Callable[[In], Out]
+    func: Callable[[In | None], Out]
 
     # We introduce 'NextOut' scoped only to this method
     def __or__[NextOut](self, other: Runnable[Out, NextOut]) -> RunnableSequence[In, Out, NextOut]:
@@ -17,8 +18,17 @@ class Runnable[In, Out]:
         # Wrap the raw callable into a Runnable to maintain the chain
         return RunnableSequence(Runnable(other), self)
 
-    def invoke(self, value: In) -> Out:
-        print(f"Calling {self.func.__name__} with value {value}")
+    def bind(self, **kwargs) -> None:
+        """Mutates the func attribute in-place with partial."""
+        self.func = partial(self.func, **kwargs)
+
+    def invoke(self, value: In | None = None) -> Out:
+        print(f"Calling Runnable with {value = }")
+        sig = inspect.signature(self.func)
+        print(f"{sig = }")
+        print(f"{self.func = }")
+        if value is None:
+            return self.func() # calls wrapper
         return self.func(value)
 
 
@@ -28,14 +38,13 @@ class RunnableSequence[In, Mid, Out](Runnable[In, Out]):
         self.first = first
         self.last = last
 
-    def invoke(self, value: In) -> Out:
-        print("Calling RunnableSequence")
+    def invoke(self, value: In | None = None) -> Out:
+        print(f"Calling RunnableSequence {value = }")
         # Static checkers now verify:
         # In -> Mid -> Out
         intermediate: Mid = self.first.invoke(value)
         print(f"Intermediate value: {intermediate}")
         final: Out = self.last.invoke(intermediate)
-        # print(f"Final value: {final}")
         return final
 
 
@@ -49,19 +58,14 @@ type RunnableFactory[In, Out] = Callable[..., Runnable[In, Out]]
 
 def runnable[In, Out](fn: RunnableFn[In, Out]) -> Runnable[In, Out]:
     @wraps(fn)
-    def wrapper(obj: In) -> Out:
-        return fn(obj)
+    def wrapper(*args: Any, **kwargs: Any) -> Out:
+        print(f"{args = }")
+        print(f"{kwargs = }")
+        sig = inspect.signature(fn)
+        print(f"{sig = }")
+        return fn(*args, **kwargs)
 
     return Runnable(wrapper)
-
-
-# Allows to pass extra params to runnable function
-def runnable_extra[In, Out](fn: RunnableDef) -> RunnableFactory[In, Out]:
-    @wraps(fn)
-    def wrapper(*args: Any, **kwargs: Any) -> Runnable[In, Out]:
-        return Runnable(lambda obj: fn(*args, obj, **kwargs)) # obj is the param received in invoke()
-
-    return wrapper
 
 @runnable
 def add_five(x: int) -> int:
@@ -71,6 +75,35 @@ def add_five(x: int) -> int:
 @runnable
 def multiply_by_two(x: int) -> int:
     return x * 2
+
+
+# Let's define some "steps"
+# Composition: No math is done yet!
+# This creates a RunnableSequence(add_five, multiply_by_two)
+runnable_sequence: RunnableSequence = add_five | multiply_by_two
+print(f"{runnable_sequence.invoke(10) = }")
+print("-----------------------------------------------------------------------")
+
+@runnable
+def multiply_three_vars(x: int, y: int, z: int) -> int:
+    return x * y * z
+
+# Partially initialize dependencies
+add_five.bind(x=5)
+multiply_three_vars.bind(y=3, z=4)
+runnable_sequence_bound: RunnableSequence = add_five | multiply_by_two | multiply_three_vars
+print(f"{type(runnable_sequence) = }")
+print(f"{runnable_sequence_bound.invoke() = }")
+print("-----------------------------------------------------------------------")
+
+# Allows to pass extra params to runnable function
+def runnable_extra[In, Out](fn: RunnableDef) -> RunnableFactory[In, Out]:
+    @wraps(fn)
+    def wrapper(*args: Any, **kwargs: Any) -> Runnable[In, Out]:
+        return Runnable(lambda obj: fn(*args, obj, **kwargs)) # obj is the param received in invoke()
+
+    return wrapper
+
 
 @runnable_extra
 def add(x: int, y: int) -> int: # y is the obj parameter of the wrapper
@@ -82,21 +115,10 @@ def multiply(x: int, y: int) -> int:
     print(f"Multiplying {x} and {y}")
     return x * y
 
-
-# Let's define some "steps"
-# Composition: No math is done yet!
-# This creates a RunnableSequence(add_five, multiply_by_two)
-runnable_sequence: RunnableSequence = add_five | multiply_by_two
-
-# print(f"{vars(runnable_sequence) = }")
-# print(f"{type(runnable_sequence) = }")
-
-print(f"{runnable_sequence.invoke(10) = }")
-
 runnable_extra_sequence = add(5) | multiply(2) # 5 and 2 are the x params, the *args
 
 print(f"{runnable_extra_sequence.invoke(10) = }") # 10 is the y param (obj of the decorator)
-
+print("-----------------------------------------------------------------------")
 
 # ------------------------------------------------------------
 # CONDITIONAL BRANCHING
@@ -128,6 +150,13 @@ def condition(value: int) -> bool:
     print(f"Checking condition value > 5 for value: {value}")
     return value > 10
 
+@runnable
+def add_five(x: int) -> int:
+    return x + 5
+
+@runnable
+def multiply_by_two(x: int) -> int:
+    return x * 2
 
 # Routing logic: If result > 50, continue processing. Else, return immediately.
 conditional = RunnableBranch(
@@ -140,6 +169,8 @@ conditional = RunnableBranch(
 conditional_pipeline: RunnableSequence[int, int, int] = add_five | conditional
 
 print(f"{conditional_pipeline.invoke(10) = }")
+print("-----------------------------------------------------------------------")
+
 
 # ------------------------------------------------------------
 # Executing multiple functions
